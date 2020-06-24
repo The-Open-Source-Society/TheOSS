3bd3044f1250a4e57eeda261a040bb8cc02c9446
Arkaprabha Chakraborty <chakrabortyarkaprabha998@gmail.com>
2020-06-24 20:20:59 +0530

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   public/app.py 	modified:   public/templates/layout.html 	new file:   public/templates/toss.html

--- public/app.py
+++ public/app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template, request
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Email, Length
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators =[InputRequired(), Length(min=4, max=50)])
+    password = PasswordField('Password', validators =[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me') 
 
+class RegisterForm(FlaskForm):
+     username = StringField('Username', validators =[InputRequired(), Length(min=4, max=50)])
+     email = StringField('Email ID', validators = [InputRequired(), Email(message='Invalid email'), Length(max=50)]) 
+     password = PasswordField('Password', validators =[InputRequired(), Length(min=8, max=80)])
 app = Flask(__name__)
 
-
 @app.route("/")
-def index():
+def signin():
 
-    return render_template("index.html")
+    return render_template("toss.html")
 
 @app.route("/contact")
 def contact():
     
     return "stheopensource@gmail.com"
+
+if __name__ == '__main__':
+    app.run(debug=True)
