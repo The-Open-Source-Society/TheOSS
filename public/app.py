5c80c616eaca999d499aa0b8ed7ac2c5e05e82eb
Arkaprabha Chakraborty <chakrabortyarkaprabha998@gmail.com>
2020-06-24 21:55:13 +0530

    Changes to be committed: 	modified:   public/app.py 	modified:   public/templates/login.html

--- public/app.py
+++ public/app.py
@@ -1,24 +1,21 @@
 from flask import Flask, render_template, request
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
-from flask_wtf import FlaskForm
-from wtforms.validators import InputRequired, Email, Length
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators =[InputRequired(), Length(min=4, max=50)])
-    password = PasswordField('Password', validators =[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('remember me') 
-
-class RegisterForm(FlaskForm):
-     username = StringField('Username', validators =[InputRequired(), Length(min=4, max=50)])
-     email = StringField('Email ID', validators = [InputRequired(), Email(message='Invalid email'), Length(max=50)]) 
-     password = PasswordField('Password', validators =[InputRequired(), Length(min=8, max=80)])
 
 app = Flask(__name__)
 
 @app.route("/")
-def signin():
+def home():
 
     return render_template("toss.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/login_val", methods =["GET","POST"])
+def login_valid():
+    email = request.form.get('username')
+    password = request.form.get('password')
+    return "email : {} password : {}".format(email,password)
 
 @app.route("/contact")
 def contact():
