2bff3c242490f03464b77bbc964a02279a15fb90
Arkaprabha Chakraborty <chakrabortyarkaprabha998@gmail.com>
2020-06-28 22:36:58 +0530

    Changes to be committed: 	modified:   public/app.py 	new file:   public/templates/signup.html

--- public/app.py
+++ public/app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request
+import firebase_admin
+from firebase_admin import credentials
+cred = credentials.Certificate("/home/arkaprabha/Desktop/theoss-a4460-firebase-adminsdk-hh09p-fd5a411e88.json")
+firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
 
@@ -17,6 +21,15 @@ def login_valid():
     password = request.form.get('password')
     return " Welcome username : {} ".format(username)
 
+@app.route("/signup")
+def signin():
+    return render_template("signup.html")
+
+@app.route("/signin_val", methods =["GET","POST"])
+def signin_valid():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return " Welcome username : {} ".format(username)
 @app.route("/contact")
 def contact():
     
