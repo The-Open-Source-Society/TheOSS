cd7cc24005f3652ec4665de949a68016c492901e
Arkaprabha Chakraborty <chakrabortyarkaprabha998@gmail.com>
2020-06-30 00:36:37 +0530

    Changes to be committed: 	modified:   public/app.py

--- public/app.py
+++ public/app.py
@@ -2,11 +2,32 @@ from flask import Flask, render_template, request
 import firebase_admin
 from firebase import firebase
 from firebase_admin import credentials, firestore
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+import json
+import os
+
+
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+
 cred = credentials.Certificate("/home/arkaprabha/Desktop/theoss-a4460-firebase-adminsdk-hh09p-fd5a411e88.json")
 default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
