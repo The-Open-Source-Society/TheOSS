485e2b2d84809bfc84cf40ec885990d4c4bf65a0
Arkaprabha Chakraborty <chakrabortyarkaprabha998@gmail.com>
2020-07-01 02:12:20 +0530

    Changes to be committed: 	modified:   public/app.py 	modified:   public/templates/toss.html

--- public/app.py
+++ public/app.py
@@ -1,22 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import firebase_admin
 from firebase import firebase
 from firebase_admin import credentials, firestore
-from flask_login import (
-    LoginManager,
-    current_user,
-    login_required,
-    login_user,
-    logout_user,
-)
 from oauthlib.oauth2 import WebApplicationClient
 import requests
 import json
 import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+
+GOOGLE_CLIENT_ID = "351414881625-vic74n66h8t6l5npsd1jooc39tqce7uo.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "LeyDU4kT231r_AdZsCNqQXO6"
 GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration"
 )
@@ -27,7 +22,11 @@ default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 @app.route("/")
 def home():
@@ -35,7 +34,50 @@ def home():
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+       return render_template("login.html")
+
+@app.route("/login_with_google")
+def logingoogle():
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+@app.route("/login_with_google/callback")
+def callback():
+    code = request.args.get("code")
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+    token_url, headers, body = client.prepare_token_request(token_endpoint,authorization_response=request.url,
+    redirect_url=request.base_url, code = code)
+    token_response = requests.post(
+    token_url,
+    headers=headers,
+    data=body,
+    auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET))
+    client.parse_request_body_response(json.dumps(token_response.json()))
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+    if userinfo_response.json().get("email_verified"):
+      users_email = userinfo_response.json()["email"]
+      picture = userinfo_response.json()["picture"]
+      users_name = userinfo_response.json()["given_name"]
+    else:
+      return "User email not available or not verified by Google.", 400
+    
+
+    if not db.get(users_email):
+        s = str(users_email)
+        doc_ref = db.collection("User").document(s)
+        doc_ref.set({'Email':users_email,'Password':users_name})
+
+
+
 
 @app.route("/login_val", methods =["GET","POST"])
 def login_valid():
@@ -52,7 +94,7 @@ def signin_valid():
     username = request.form.get('name')
     password = request.form.get('password')
     email = request.form.get('email')
-    s = str(username)
+    s = str(email)
     doc_ref = db.collection("User").document(s)
     doc_ref.set({'Name':username,'Email':email,'Password':password})
     return " Welcome username : {} ".format(username)
