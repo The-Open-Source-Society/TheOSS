3517a18c24a8b4cb599e798a8a4274180c3e0e93
Arkaprabha Chakraborty <chakrabortyarkaprabha998@gmail.com>
2020-07-01 21:55:19 +0530

    Changes to be committed: 	modified:   public/app.py 	modified:   public/templates/toss.html

--- public/app.py
+++ public/app.py
@@ -11,7 +11,7 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 
 GOOGLE_CLIENT_ID = "351414881625-vic74n66h8t6l5npsd1jooc39tqce7uo.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "LeyDU4kT231r_AdZsCNqQXO6"
+GOOGLE_CLIENT_SECRET = "SwcaRytS4y0tghchcOyOq3BI"
 GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration"
 )
@@ -67,15 +67,10 @@ def callback():
       users_email = userinfo_response.json()["email"]
       picture = userinfo_response.json()["picture"]
       users_name = userinfo_response.json()["given_name"]
+      return "Successfull"
     else:
       return "User email not available or not verified by Google.", 400
-    
-
-    if not db.get(users_email):
-        s = str(users_email)
-        doc_ref = db.collection("User").document(s)
-        doc_ref.set({'Email':users_email,'Password':users_name})
-
+    return redirect(url_for("home"))
 
 
 
@@ -98,6 +93,46 @@ def signin_valid():
     doc_ref = db.collection("User").document(s)
     doc_ref.set({'Name':username,'Email':email,'Password':password})
     return " Welcome username : {} ".format(username)
+
+
+@app.route("/signup_with_google")
+def signupgoogle():
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+@app.route("/signup_with_google/callback")
+def signupcallback():
+    code = request.args.get("code")
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+    token_url, headers, body = client.prepare_token_request(token_endpoint,authorization_response=request.url,
+    redirect_url=request.base_url, code = code)
+    token_response = requests.post(
+    token_url,
+    headers=headers,
+    data=body,
+    auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET))
+    client.parse_request_body_response(json.dumps(token_response.json()))
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+    if userinfo_response.json().get("email_verified"):
+      users_email = userinfo_response.json()["email"]
+      picture = userinfo_response.json()["picture"]
+      users_name = userinfo_response.json()["given_name"]
+      s = str(users_email)
+      doc_ref = db.collection("User").document(s)
+      doc_ref.set({'Name':users_name,'Email':users_email,'Password':users_name})
+      return " Welcome username : {} ".format(users_name)
+    else:
+      return "User email not available or not verified by Google.", 400
+    return redirect(url_for("home"))
 
 @app.route("/contact")
 def contact():
