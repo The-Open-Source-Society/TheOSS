bb2bd68db741cf9400b2fd48f800f30306124510
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-07-01 22:38:47 +0530

    bug fix

--- public/app.py
+++ public/app.py
@@ -18,9 +18,9 @@ GOOGLE_DISCOVERY_URL = (
 )
 
 
-cred = credentials.Certificate("/home/arkaprabha/Desktop/theoss-a4460-firebase-adminsdk-hh09p-fd5a411e88.json")
-default_app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+# cred = credentials.Certificate("/home/arkaprabha/Desktop/theoss-a4460-firebase-adminsdk-hh09p-fd5a411e88.json")
+# default_app = firebase_admin.initialize_app(cred)
+# db = firestore.client()
 
 from flask import Flask, render_template, request
 
